7d542163f58db5f8e88f59d5e64a5f255418b2bd
Lucas <542241425@qq.com>
2017-09-12 22:07:28 +0800

    backgammon
    backgammon

---
+++ js/main.js
@@ -0,0 +1,322 @@
+var canvas = document.getElementById('canvas'),
+	ctx = canvas.getContext('2d'),
+	width = canvas.width,							//the height of canvas
+	height = canvas.height, 						//the width of canvas
+	chessR = 20,									//chess radius
+	commonConstant = 1 + chessR,					//inital position of the chess board data
+ 	positionX =	parseInt((width-chessR*2)/18), 		//the average length of X axios
+	positionY =	parseInt((height-chessR*2)/18),		//the average length of Y axios
+	data = [],										//chess board data
+	wins = [],										//all the situation of victories
+	myWins = [],									//the probability of player1 winning
+	AIWins = [],									//the probability of AI winning
+	count = 0,										//the number of victories
+	player1 = new Player1('black'),					//the instance of player1
+	player2 = new Player2('ghostwhite'),			//the instance of player2
+	computer = new Computer('ghostwhite'), 			//the instance of AI
+	flag = true,									//the mark of playing chess(true represent player1;false represent AI)
+	over = false;									//judge game over
+	
+	
+//structure function player1
+function Player1(color){
+	this.color = color;
+}
+
+//structure function player2
+function Player2(color){
+	this.color = color;
+}
+
+//structure function AI
+function Computer(color){
+	this.color = color;
+}
+
+
+//init chessboarddata 
+(function initChessBoardData(data){
+	for (let i = 0; i < 19; i++) {
+		data[i] = [];
+		for (let j = 0; j < 19; j++) {
+			data[i][j] = 2 ;
+		}
+	}
+	return data;
+})(data);
+
+
+//init chessboard
+(function drawCheckboard(){
+	ctx.strokeStyle = 'black';
+	ctx.fillStyle = 'black';
+	ctx.lineWidth = 2;
+	for (let i = commonConstant; i <= width - commonConstant; i += positionX) {
+		ctx.beginPath();
+		ctx.moveTo(i,commonConstant);
+		ctx.lineTo(i,height-commonConstant);
+		ctx.closePath();
+		ctx.stroke();
+	}	
+	for (let j = commonConstant; j <= height - commonConstant; j += positionY) {
+		ctx.beginPath();
+		ctx.moveTo(commonConstant,j);
+		ctx.lineTo(width - commonConstant,j);
+		ctx.closePath();
+		ctx.stroke();
+	}	
+	for (let k = positionX * 3 + commonConstant; k <= positionX * 15 + commonConstant; k += positionX * 6) {
+		for (let l = positionY * 3 + commonConstant; l <= positionY * 15 + commonConstant; l += positionY * 6) {
+		ctx.beginPath();
+		ctx.arc(k,l,7,0,Math.PI*2,true);
+		ctx.closePath();
+		ctx.fill();
+		}
+	}	
+})();
+
+
+//init the data of victories 
+(function initWinningData(){
+	for (let i = commonConstant; i <= width - commonConstant; i += positionX) {
+		wins[parseInt(i/positionX)] = [];
+		for (let j = commonConstant; j <= height - commonConstant; j += positionY) {
+			wins[parseInt(i/positionX)][parseInt(j/positionY)] = [];
+		}
+	}
+	
+	//horizontal data
+	for (var i = commonConstant; i <= width - commonConstant; i += positionX) {
+		for (var j = commonConstant; j <= height - commonConstant - 4 * positionY; j += positionY) {
+			for (var k = 0; k < 5; k++) {
+				wins[parseInt(i/positionX)][parseInt(j/positionY) + k][count] = true;
+			}
+			count++;
+		}
+	}
+	
+	//vertical data
+	for (var i = commonConstant; i <= width - commonConstant; i += positionX) {
+		for (var j = commonConstant; j <= height - commonConstant - 4 * positionY; j += positionY) {
+			for (var k = 0; k < 5; k++) {
+				wins[parseInt(j/positionY) + k][parseInt(i/positionX)][count] = true;
+			}
+			count++;
+		}
+	}
+	
+	//oblique data
+	for (var i = commonConstant; i <= width - commonConstant - 4 * positionX; i += positionX) {
+		for (var j = commonConstant; j <= height - commonConstant - 4 * positionY; j += positionY) {
+			for (var k = 0; k < 5; k++) {
+				wins[parseInt(i/positionX) + k][parseInt(j/positionY) + k][count] = true;
+			}
+			count++;
+		}
+	}
+	
+	//anti-oblique data
+	for (var i = commonConstant; i <= width - commonConstant - 4 * positionX; i += positionX) {
+		for (var j = height - commonConstant - positionY; j > commonConstant + 3 * positionY; j -= positionY) {
+			for (var k = 0; k < 5; k++) {
+				wins[parseInt(i/positionX) + k][parseInt(j/positionY) - k][count] = true;
+			}
+			count++;
+		}
+	}
+	
+	//init the probability of winning
+	for (var i = 0; i < count; i++) {
+		myWins[i] = 0;
+		AIWins[i] = 0;
+	}
+	
+})();
+
+
+
+//get mouse position
+function getCanvasPos(canvas,e){   
+    var rect = canvas.getBoundingClientRect();
+    return {   
+     x: (e.clientX - rect.left) * (canvas.width / rect.width),  
+     y: (e.clientY - rect.top) * (canvas.height / rect.height)   
+   };  
+}
+
+
+//player1 op
+Player1.prototype.drawChess = function(e){
+		ctx.fillStyle = this.color;	
+		let pos = getCanvasPos(canvas,e);
+		for (let i = commonConstant; i <= width - commonConstant; i += positionX) {
+			for (let j = commonConstant; j <= height - commonConstant; j += positionY) {
+				if((pos.x - i) * (pos.x - i) + (pos.y - j) * (pos.y - j) < 100){
+					if(data[parseInt(i/positionX)][parseInt(j/positionY)] == 2){
+					ctx.beginPath();
+					ctx.arc(i,j,chessR,0,Math.PI*2,true);
+					ctx.closePath();
+					ctx.strokeStyle = 'black';
+					ctx.stroke();				
+					ctx.fill();
+					data[parseInt(i/positionX)][parseInt(j/positionY)] = 0;
+					for (let k = 0; k < count; k++) {
+						if(wins[parseInt(i/positionX)][parseInt(j/positionY)][k]){
+							myWins[k]++;
+							AIWins[k] = 99;
+							if(myWins[k] == 5){
+								over = true;
+								setTimeout(function(){
+									alert('You WIN!');
+								},500);
+							}
+						}
+					}
+					if(!isGameover() && !over){
+						flag = false;
+						computer.AI();
+					}
+					}
+				}
+			}
+		}
+};
+
+
+
+//player2 op
+Player2.prototype.drawChess = function(e){
+		ctx.fillStyle = this.color;
+		let pos = getCanvasPos(canvas,e);
+		for (let i = commonConstant; i <= width - commonConstant; i += positionX) {
+			for (let j = commonConstant; j <= height - commonConstant; j += positionY) {
+				if((pos.x - i) * (pos.x - i) + (pos.y - j) * (pos.y - j) < 100){
+					if(data[parseInt(i/positionX)][parseInt(j/positionY)] == 2){
+					ctx.beginPath();
+					ctx.arc(i,j,chessR,0,Math.PI*2,true);
+					ctx.closePath();
+					ctx.strokeStyle = 'black';
+					ctx.stroke();					
+					ctx.fill();
+					data[parseInt(i/positionX)][parseInt(j/positionY)] = 1;
+					flag = true;
+					}
+				}	
+			}
+		}	
+}
+
+//AI op
+Computer.prototype.AI = function(){
+	var myScore = [],	//player1 score
+	 	AIScore = [],	//AI score
+	 	maxScore = 0,	//the worse score
+	 	m = 0, 			//the best X position
+	 	n = 0;			//the best Y position
+	 	
+	for (let i = commonConstant; i <= width - commonConstant; i += positionX) {
+	 	myScore[parseInt(i/positionX)] = [];
+		AIScore[parseInt(i/positionX)] = [];
+		for (let j = commonConstant; j <= height - commonConstant; j += positionY) {
+			myScore[parseInt(i/positionX)][parseInt(j/positionY)] = 0;
+			AIScore[parseInt(i/positionX)][parseInt(j/positionY)] = 0;
+		}
+	}
+	
+	for (let i = commonConstant; i <= width - commonConstant; i += positionX) {
+		for (let j = commonConstant; j <= height - commonConstant; j += positionY) {
+			if(data[parseInt(i/positionX)][parseInt(j/positionY)] == 2){
+				for (let k = 0; k < count; k++) {
+					if(wins[parseInt(i/positionX)][parseInt(j/positionY)][k]){
+						if(myWins[k] == 1){
+							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 100;
+						}else if(myWins[k] == 2){
+							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 200;
+						}else if(myWins[k] == 3){
+							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 2000;
+						}else if(myWins[k] == 4){
+							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 20000;
+						}
+						if(AIWins[k] == 1){
+							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 150;
+						}else if(AIWins[k] == 2){
+							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 450;
+						}else if(AIWins[k] == 3){
+							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 2500;
+						}else if(AIWins[k] == 4){
+							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 50000;
+						}
+					}
+				}
+				
+				if(myScore[parseInt(i/positionX)][parseInt(j/positionY)] > maxScore){
+					maxScore = myScore[parseInt(i/positionX)][parseInt(j/positionY)];
+					m = i;
+					n = j;
+				}else if(myScore[parseInt(i/positionX)][parseInt(j/positionY)] == maxScore){
+					if(AIScore[parseInt(i/positionX)][parseInt(j/positionY)] > AIScore[parseInt(m/positionX)][parseInt(n/positionY)]){
+						m = i; 
+						n = j;
+					}
+				}
+				
+				if(AIScore[parseInt(i/positionX)][parseInt(j/positionY)] > maxScore){
+					maxScore = AIScore[parseInt(i/positionX)][parseInt(j/positionY)];
+					m = i;
+					n = j;
+				}else if(AIScore[parseInt(i/positionX)][parseInt(j/positionY)] == maxScore){
+					if(myScore[parseInt(i/positionX)][parseInt(j/positionY)] > myScore[parseInt(m/positionX)][parseInt(n/positionY)]){
+						m = i;
+						n = j;
+					}
+				}
+			}
+		}
+	}
+	ctx.beginPath();
+	ctx.arc(m,n,chessR,0,Math.PI*2,true);
+	ctx.closePath();
+	ctx.strokeStyle = 'black';
+	ctx.stroke();
+	ctx.fillStyle = this.color;
+	ctx.fill();
+	data[parseInt(m/positionX)][parseInt(n/positionY)] = 1;	
+	for (let k = 0; k < count; k++) {
+		if(wins[parseInt(m/positionX)][parseInt(n/positionY)][k]){
+		AIWins[k]++;
+		myWins[k] = 99;
+			if(AIWins[k] == 5){
+				over = true;
+				setTimeout(function(){
+					alert('AI WIN!');
+				},500);
+			}
+		}
+	}
+	if(!isGameover() && !over){
+		flag = true;
+	}
+}
+
+
+//game start
+$('#canvas').on('click',function(e){
+	if(flag){
+		player1.drawChess(e);
+	}else{
+		return;
+	}
+});
+
+
+//game rule(0 represent black chess, 1 represent white chess, 2 represent space)
+function isGameover(){
+	for (let i = commonConstant; i <= width - commonConstant; i += positionX) {
+		for (let j = commonConstant; j <= height - commonConstant; j += positionY) {
+			if(data[parseInt(i/positionX)][parseInt(j/positionY)] == 2){
+				return false;
+			}
+		}
+	}
+	return true;
+}
