01d9019ef20a0e1deef406210e75a692de6942e6
Lucas <542241425@qq.com>
2017-09-13 20:04:07 +0800

    backgammon
    much smarter

--- js/main.js
+++ js/main.js
@@ -85,46 +85,47 @@ function Computer(color){
 		}
 	}
 	
-	//horizontal data
+	//horizontal data(k:0-285)
 	for (var i = commonConstant; i <= width - commonConstant; i += positionX) {
 		for (var j = commonConstant; j <= height - commonConstant - 4 * positionY; j += positionY) {
 			for (var k = 0; k < 5; k++) {
-				wins[parseInt(i/positionX)][parseInt(j/positionY) + k][count] = true;
+				wins[parseInt(i/positionX)][parseInt(j/positionY) + k][count] = count + 1;
 			}
 			count++;
 		}
 	}
+	console.log(count);
 	
-	//vertical data
+	//vertical data(k:286-570)
 	for (var i = commonConstant; i <= width - commonConstant; i += positionX) {
 		for (var j = commonConstant; j <= height - commonConstant - 4 * positionY; j += positionY) {
 			for (var k = 0; k < 5; k++) {
-				wins[parseInt(j/positionY) + k][parseInt(i/positionX)][count] = true;
+				wins[parseInt(j/positionY) + k][parseInt(i/positionX)][count] = count + 1;
 			}
 			count++;
 		}
 	}
-	
-	//oblique data
+	console.log(count);
+	//oblique data(k:571-795)
 	for (var i = commonConstant; i <= width - commonConstant - 4 * positionX; i += positionX) {
 		for (var j = commonConstant; j <= height - commonConstant - 4 * positionY; j += positionY) {
 			for (var k = 0; k < 5; k++) {
-				wins[parseInt(i/positionX) + k][parseInt(j/positionY) + k][count] = true;
+				wins[parseInt(i/positionX) + k][parseInt(j/positionY) + k][count] = count + 1;
 			}
 			count++;
 		}
 	}
-	
-	//anti-oblique data
+	console.log(count);
+	//anti-oblique data(796-1020)
 	for (var i = commonConstant; i <= width - commonConstant - 4 * positionX; i += positionX) {
-		for (var j = height - commonConstant - positionY; j > commonConstant + 3 * positionY; j -= positionY) {
+		for (var j = height - commonConstant; j >= commonConstant + 4 * positionY; j -= positionY) {
 			for (var k = 0; k < 5; k++) {
-				wins[parseInt(i/positionX) + k][parseInt(j/positionY) - k][count] = true;
+				wins[parseInt(i/positionX) + k][parseInt(j/positionY) - k][count] = count + 1;
 			}
 			count++;
 		}
 	}
-	
+	console.log(count);	
 	//init the probability of winning
 	for (var i = 0; i < count; i++) {
 		myWins[i] = 0;
@@ -147,7 +148,8 @@ function getCanvasPos(canvas,e){
 
 //player1 op
 Player1.prototype.drawChess = function(e){
-		ctx.fillStyle = this.color;	
+		ctx.fillStyle = this.color;
+		let m = 0,n = 0;
 		let pos = getCanvasPos(canvas,e);
 		for (let i = commonConstant; i <= width - commonConstant; i += positionX) {
 			for (let j = commonConstant; j <= height - commonConstant; j += positionY) {
@@ -159,9 +161,11 @@ Player1.prototype.drawChess = function(e){
 					ctx.strokeStyle = 'black';
 					ctx.stroke();				
 					ctx.fill();
-					data[parseInt(i/positionX)][parseInt(j/positionY)] = 0;
+					m = parseInt(i/positionX);
+					n = parseInt(j/positionY);
+					data[m][n] = 0;
 					for (let k = 0; k < count; k++) {
-						if(wins[parseInt(i/positionX)][parseInt(j/positionY)][k]){
+						if(wins[m][n][k]){
 							myWins[k]++;
 							AIWins[k] = 99;
 							if(myWins[k] == 5){
@@ -225,6 +229,12 @@ Computer.prototype.AI = function(){
 	
 	for (let i = commonConstant; i <= width - commonConstant; i += positionX) {
 		for (let j = commonConstant; j <= height - commonConstant; j += positionY) {
+			let myCount = 0,
+				AICount = 0,
+				myCount_1 = 0,
+				AICount_1 = 0,
+				u = parseInt(i/positionX),
+				v = parseInt(j/positionY);
 			if(data[parseInt(i/positionX)][parseInt(j/positionY)] == 2){
 				for (let k = 0; k < count; k++) {
 					if(wins[parseInt(i/positionX)][parseInt(j/positionY)][k]){
@@ -232,19 +242,53 @@ Computer.prototype.AI = function(){
 							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 100;
 						}else if(myWins[k] == 2){
 							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 200;
+							for (let w = 0; w < wins[u][v].length; w++) {
+								if(myWins[wins[u][v][w]-1] == 2){
+									myCount++;
+								}
+								if(myWins[wins[u][v][w]-1] == 99){
+									myCount--;
+								}	
+							}
+							if(myCount >= 2){
+								myScore[u][v] += 3000;
+							}
 						}else if(myWins[k] == 3){
-							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 2000;
+							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 13000;
+							for (let w = 0; w < wins[u][v].length; w++) {
+								if(myWins[wins[u][v][w]-1] == 99 && wins[u][v][w]-1 == k){
+									myScore[parseInt(i/positionX)][parseInt(j/positionY)] -= 6000;
+								}	
+							}
+							
 						}else if(myWins[k] == 4){
-							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 20000;
+							myScore[parseInt(i/positionX)][parseInt(j/positionY)] += 66666;
 						}
 						if(AIWins[k] == 1){
 							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 150;
 						}else if(AIWins[k] == 2){
 							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 450;
+							for (let w = 0; w < wins[u][v].length; w++) {
+								if(AIWins[wins[u][v][w]-1] == 2){
+									AICount++;
+								}
+								if(AIWins[wins[u][v][w]-1] == 99){
+									AICount--;
+								}	
+							}
+							if(AICount >= 2){
+								AIScore[u][v] += 5000;
+							}
 						}else if(AIWins[k] == 3){
-							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 2500;
+							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 20000;
+							for (let w = 0; w < wins[u][v].length; w++) {
+								if(AIWins[wins[u][v][w]-1] == 99 && wins[u][v][w]-1 == k){
+									AICount[parseInt(i/positionX)][parseInt(j/positionY)] -= 10000;
+								}	
+							}
+							
 						}else if(AIWins[k] == 4){
-							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 50000;
+							AIScore[parseInt(i/positionX)][parseInt(j/positionY)] += 99999;
 						}
 					}
 				}
@@ -320,3 +364,4 @@ function isGameover(){
 	}
 	return true;
 }
+
